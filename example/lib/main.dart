db3fd70dcc483d662ee80158e203253178a90431
Bao Tran <baotv@holitech.vn>
2025-11-25 09:07:37 +0700

    add demo

---
+++ example/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:awesome_utils/awesome_utils.dart';
+
+void main() {
+  print(add(2, 3)); // 5
+}
---
+++ lib/awesome_utils.dart
@@ -0,0 +1,9 @@
+/// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+library awesome_utils;
+
+int add(int a, int b) => a + b;
